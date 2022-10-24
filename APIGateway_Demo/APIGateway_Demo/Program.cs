3205739bf3305238e17d2eaae2e062e6a62020ab
phamtuandatt <109933954+phamtuandatt@users.noreply.github.com>
2022-10-24 13:58:05 +0700

    add nlog.config, edit Program.cs

--- APIGateway_Demo/APIGateway_Demo/Program.cs
+++ APIGateway_Demo/APIGateway_Demo/Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,21 @@ namespace APIGateway_Demo
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("Init main function");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Error in init");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -24,6 +40,11 @@ namespace APIGateway_Demo
                     webBuilder.UseStartup<Startup>();
                     webBuilder.ConfigureAppConfiguration(config =>
                     config.AddJsonFile($"ocelot.{env}.json"));
-                }).ConfigureLogging(loggin => loggin.AddConsole());
+                }).ConfigureLogging(loggin =>
+                {
+                    loggin.ClearProviders();
+                    loggin.SetMinimumLevel(LogLevel.Information);
+                    loggin.AddConsole();
+                }).UseNLog();
     }
 }
